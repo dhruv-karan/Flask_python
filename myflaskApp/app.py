9f83881a3d5e69bcae102d474af392d32e7e5144
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-06 13:32:49 +0530

    connected model with server

--- myflaskApp/app.py
+++ myflaskApp/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
+model = pickle.load(open("model.pkl","rb"))
+arr = np.array([[5,69]])
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/predict')
+def predict():
+     if(model.predict(arr >0.5)):
+         return 'male'
+     else:
+         return 'femlae'
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
