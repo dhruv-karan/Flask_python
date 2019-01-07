2fe0049a2be5cc317585268c86641160568f7cd6
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-07 23:11:27 +0530

    form logic added via wtforms

--- gender_classifier/app.py
+++ gender_classifier/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
-from wtforms import Form, TextAreaField, validators
+from wtforms import Form, TextAreaField, validators, IntegerField
 
 import pickle
 import os
-import sqlite3 
+import sqlite3
 import numpy as np
 
 app = Flask(__name__)
@@ -14,42 +14,63 @@ clf = pickle.load(open(os.path.join(cur_dir,'model.pkl'),'rb'))
 db = os.path.join(cur_dir,'reviews.sqlite')
 
 
-def classifier(document):
+def classifier(height,weight):
     label = {0:'female',1:'male'}
-    X = np.array((document))
+    X = np.array((height,weight))
     y = clf.predict(X)[0]
-    if y <0.5:
-        y ==0
+    if y < 0.5:
+        y == 0
     else:
-        y ==1
+        y == 1
     return label[y]
-def train(document,y):
-    X = np.array([[document]])
+
+def train(height,weight,y):
+    X = np.array([[height,weight]])
     clf.partial_fit(X,y)
 
-def sqlite_entry(path,document,y):
+def sqlite_entry(path,height,weight,y):
     conn = sqlite3.connect(path)
     c = conn.cursor()
-    c.execute("INSERT INTO review_db (review,sentiment,date)"\
-    "VALUES(?,?,DATETIME('now'))",(document,y))
+    c.execute("INSERT INTO review_db (height,weight,gender,date)"\
+    "VALUES(?,?,?,DATETIME('now'))",(height,weight,y))
     conn.commit()
     conn.close()
 
 
-#================================== routing or can be seen logic 
-@app.route('/')
-def index():
-    return render_template('home.html')
+#================================== routing or can be seen logic
+class Dataform(Form):
+    height = IntegerField('height', [validators.length(max=3, min=2)] )
+    weight =  IntegerField('height', [validators.length(max=3, min=2)] )
     
 
+@app.route('/')
+def index():
+    form = Dataform(request.form)
+    return render_template('home.html', form=form)
 
-@app.route('/predict')
-def predict():
-     if(model.predict(arr >0.5)):
-         return 'male'
-     else:
-         return 'femlae'
 
+@app.route('/predict',methods=['POST'])
+def result():
+    form  = Dataform(request.form)
+    if request.method == 'POST' and form.validate():
+        height = request.form['height']
+        weight = request.form['weight']
+        y = classifier(height,weight)
+        return render_template('predict.html', height = height, weight=weight,prediction=y)
+  
+@app.route('/thanks',methods=['POST'])
+def feedback():
+    feedback = request.form('feedback_button')
+    height = request.form['height']
+    weight = request.form['weight']
+    prediction = request.form['prediction']
+    inv_label = {'female':0,'male':1}
+    y = inv_label[prediction]
+    if feedback == 'Incorrect':
+        y = int(not(y))
+    train(height,weight,y)
+    sqlite_entry(db,height,weight,y)
+    return render_template('thanks.html')
 
 
 if __name__ == "__main__":
