b74e7fd7d2a7983b0ccfcc19596cad4c5e5d5492
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-12 23:57:16 +0530

    app structure started

--- text_analysier/app.py
+++ text_analysier/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators, IntegerField
+
+import pickle
+import os
+import sqlite3
+import numpy as np
 
 app = Flask(__name__)
+cur_dir = os.path.dirname(__file__)
+model = pickle.load(open("model.pkl","rb"))
+clf = pickle.load(open(os.path.join(cur_dir,'model.pkl'),'rb'))
+
+db = os.path.join(cur_dir,'reviews.sqlite')
 
 @app.route('/')
 def index():
