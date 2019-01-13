e478722a75f71d95c4bc9b634094d9611dc0431b
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-13 12:59:36 +0530

    automated preprocesser is left

--- text_analysier/app.py
+++ text_analysier/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
-from wtforms import Form, TextAreaField, validators, IntegerField
+from wtforms import Form, TextAreaField, validators
 from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import stopwords
+
+# from nltk.corpus import stopwords
+import re
 
 
 import pickle
@@ -16,7 +18,7 @@ clf = pickle.load(open(os.path.join(cur_dir,'model.pkl'),'rb'))
 db = os.path.join(cur_dir,'reviews.sqlite')
 
 def clean(review):
-    review = re.sub(r'\W', ' ', str(data_x[i]))
+    review = re.sub(r'\W', ' ', review)
     review = re.sub(r'[0-9]', ' ', review)
     review = review.lower()
     review = re.sub(r'^br$', ' ', review)
@@ -24,13 +26,21 @@ def clean(review):
     review = re.sub(r'\s+[a-z]\s+', ' ',review)
     review = re.sub(r'^b\s+', '', review)
     review = re.sub(r'\s+', ' ', review)
-    vectorizer = TfidfVectorizer(stop_words = stopwords.words('english'))
-    return review = vectorizer.toarray()
+    return review
 
+def toArray(review):
+    corpus = [review]
+    # vectorizer = TfidfVectorizer(stop_words = stopwords.words('english'))
+    vectorizer = TfidfVectorizer()
+    review  = vectorizer.fit_transform(corpus).toarray()
+    # review = np.reshape(review,(1,2000))
+    # review = np.array[[vectorizer]]
+    return review
 
 def classifier(review):
     label = {0:"negative",1:"positive"}
-    X = clean(review)
+    #review = clean(review)
+    X = toArray(review)
     y = clf.predict(X)
     return label[y]
 
@@ -42,12 +52,41 @@ def sqlite_entry(path,review,y):
     conn.commit()
     conn.close()
 
+#===================== routing nd pages=====================
 
-
+class Reviewform(Form):
+    review = TextAreaField('review')
 
 @app.route('/')
 def index():
-    return "We gonna buuild text analyiseireeiyfuer"
+    form = Reviewform(request.form)
+    return render_template('prediction_form.html', form = form)
+
+@app.route('/result',methods=['POST'])
+def result():
+    form = Reviewform(request.form)
+    if request.method == 'POST':
+        review = request.form['review']
+        y = classifier(review)
+        return render_template('result.html',
+        review= review,prediction=y)
+    else:
+        return render_template('prediction_form.html', form = form)
+
+# @app.route('./thanks',methods=['POST'])
+# def feedback():
+#     feedback = request.form('feedback_button')
+#     review = request.form['review']
+#     prediction = request.form['prediction']
+#     inv_label = {'negative':0,'positive':1}
+#     y = inv_label[prediction]
+#     if feedback == 'Incorrect':
+#         y = int(not(y))
+#     # train(review,y)
+#     sqlite_entry(db,review,y)
+#     return render_template('thanks.html')
+
+
 
 if __name__ == "__main__":
-    # app.run(debug=True)
+    app.run(debug=True)
